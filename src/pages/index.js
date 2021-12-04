fcb75f18e9d70bc4d4ed214e929a190ac35df4d2
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-04 21:35:59 +0530

    minor changes for favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,27 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Head from 'next/head'
+
 
 const Home = () => {
   return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      <Acomplishments />
-    </Layout>
+    <>
+      <Head>
+        <title>Abhishek Bhatt</title>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <Layout>
+        <Section grid>
+          <Hero />
+          <BgAnimation />
+        </Section>
+        <Projects />
+        <Technologies />
+        <Timeline />
+        <Acomplishments />
+      </Layout>
+    </>
   );
 };
 
